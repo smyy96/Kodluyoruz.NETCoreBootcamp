970ce24a8f10416b8a3333fd7901411361d235a9
smyy <smyy.cskn@hotmail.com>
2022-03-25 14:10:17 +0300

    PrimeNumbers first commit

---
+++ Week1/PrimeNumbers/PrimeNumbers/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace PrimeNumbers
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // Kullanıcıdan alınan sayı asal mı değil mi ekrana yazdıran program.
+        }
+    }
+}
