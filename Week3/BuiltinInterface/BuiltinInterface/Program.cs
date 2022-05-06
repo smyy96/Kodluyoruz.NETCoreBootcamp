d759e2b0f2488cc98ef121849f9cd1841274f0fd
smyy <smyy.cskn@hotmail.com>
2022-05-06 18:37:35 +0300

    IEnumerable,  CompareTo

---
+++ Week3/BuiltinInterface/BuiltinInterface/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+
+namespace BuiltinInterface
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Student student1 = new Student { Id = 1, Age = 42, AverageScore = 72, Name = "Türkay", LastName = "Ürkmez" };
+            Student student2 = new Student { Id = 2, Age = 26, AverageScore = 45, Name = "Mohammed Al", LastName = "Tawil" };
+            Student student3 = new Student { Id = 3, Age = 23, AverageScore = 100, Name = "Sümeyye", LastName = "Coşkun" };
+
+            ClassRoom classRoom = new ClassRoom();
+            classRoom.AddStudent(student1);
+            classRoom.AddStudent(student2);
+            classRoom.AddStudent(student3);
+
+
+            classRoom.Sort();
+
+            foreach (Student item in classRoom)//yield sayesinde sınıf içindeki her bir öğrenciyi tek tek aldım, ClassRoom sınıfına IEnumerable'ı implemente ettikten sonra bu işlemi gercekleştirebiliyoruz.  foreach (var item in classRoom.GetStudents) implemente etmeseydik bu sekilde alabiliyorduk verileri
+            {
+                Console.WriteLine($" {item.Name} {item.LastName} {item.AverageScore} {item.Age}");
+            }
+        }
+    }
+}
---
+++ Week3/BuiltinInterface/BuiltinInterface/Student.cs
@@ -0,0 +1,65 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BuiltinInterface
+{
+    public class Student :IComparable //IComparable interface, karşılastırma işlemi yapabilmek için kullanılır sort işleminde
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public string LastName { get; set; }
+        public int Age { get; set; }
+        public double AverageScore{ get; set; }
+
+        public int CompareTo(object obj) // student içerisndeki ögrencilerin averageScorelarında kıyaslama yapıyor
+        {
+            var that = (Student)obj;
+            if (this.AverageScore > that.AverageScore)
+            {
+                return 1;
+            }
+            else if (this.AverageScore < that.AverageScore)
+            {
+                return -1;
+            }
+            else
+            {
+                return 0;
+            }
+        }
+    }
+
+    public class ClassRoom : IEnumerable // foreach ile dönebilmenin en temel gereksinimi Ienumerable lı implement ederek saglayabiliyoruz
+    {
+        private List<Student> students = new List<Student>();
+
+        public void AddStudent(Student student) // Ögrenci ekleme
+        {
+            students.Add(student);
+        }
+
+        public IEnumerator GetEnumerator()
+        {
+            foreach (var item in students)
+            {
+                yield return item;
+            }
+        }
+
+        public List<Student> GetStudents() //Ögrencileri almak
+        {
+            return students;
+        }
+
+        public void Sort()// Sıralama
+        {
+            students.Sort();
+        }
+
+
+    }
+}
