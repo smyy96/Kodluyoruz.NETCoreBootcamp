ee7bae4cd01ad61ac3fcfb72cff040025fb9144a
smyy <smyy.cskn@hotmail.com>
2022-05-09 20:17:44 +0300

    Extension Metot

---
+++ Week4/ExtensionMethods/ExtensionMethods/MyExtensions.cs
@@ -0,0 +1,98 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ExtensionMethods
+{
+    public static class MyExtensions
+    {
+        public static double GetSquare(this int value)//this bu metotun extension metot oldugunu vurguluyor. int tipine bir metot eklemiş olduk
+        {
+            return Math.Pow(value, 2);
+        }
+        
+        public static double GetSquare(this int value, double power)
+        {
+            return Math.Pow(value, power);
+        }
+
+
+        public static int TotalWorkDays(this DateTime dateTime)
+        {
+            DateTime startedDate = new DateTime(dateTime.Year, 1, 1);
+            DateTime endDate = new DateTime(dateTime.Year, 12, 31);
+
+            int totalWorkDays = 0;
+            for (DateTime current = startedDate; current <= endDate; current= current.AddDays(1))
+            {
+                if (current.DayOfWeek!=DayOfWeek.Sunday && current.DayOfWeek != DayOfWeek.Saturday)
+                {
+                    totalWorkDays++;
+                }
+            }
+
+            return totalWorkDays;
+
+        }
+        
+        
+        
+        
+        public static int TotalWorkDays(this DateTime dateTime, List<DateTime> holidays)
+        {
+            DateTime startedDate = new DateTime(dateTime.Year, 1, 1);
+            DateTime endDate = new DateTime(dateTime.Year, 12, 31);
+
+            int totalWorkDays = 0;
+            for (DateTime current = startedDate; current <= endDate; current= current.AddDays(1))
+            {
+                if (current.DayOfWeek!=DayOfWeek.Sunday && current.DayOfWeek != DayOfWeek.Saturday)
+                {
+                    totalWorkDays++;
+                }
+
+                //foreach (var holiday in holidays)
+                //{
+                //    if (current.Month == holiday.Month && current.Day == holiday.Day)
+                //    {
+                //        totalWorkDays++;
+                //    }
+
+                //}
+
+                holidays.ForEach(holiday =>
+                {
+                    if(current.Month == holiday.Month && current.Day == holiday.Day)
+                    {
+                        totalWorkDays--;
+                    }
+
+                });
+            }
+
+            return totalWorkDays;
+
+        }
+
+        public static string NextLetter(this Random random)
+        {
+            int number = new Random().Next(65, 91);
+            return ((char)number).ToString();
+        }
+
+
+        public static string NextWord(this Random random, int length = 3)
+        {
+            string result = string.Empty;
+            for (int i = 0; i < length; i++)
+            {
+                result += random.NextLetter();
+            }
+
+            return result;
+        }
+
+    }
+}
---
+++ Week4/ExtensionMethods/ExtensionMethods/Program.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace ExtensionMethods
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int x = 4;
+            Console.WriteLine(x.GetSquare());
+
+
+
+            List<DateTime> holidays = new List<DateTime>
+            {
+                new DateTime(2022,1,1),
+                new DateTime(2022,4,23),
+                new DateTime(2022,5,2),
+                new DateTime(2022,5,3),
+                new DateTime(2022,5,4),
+                new DateTime(2022,5,19)
+            };
+
+
+            // bir yıl içinde toplam çalışılan ögün sayısı
+            Console.WriteLine(DateTime.Now.TotalWorkDays(holidays));
+
+
+
+
+            Random random = new Random();
+            Console.WriteLine(random.NextLetter());
+            Console.WriteLine(random.NextWord(5));
+;
+
+        }
+    }
+}
